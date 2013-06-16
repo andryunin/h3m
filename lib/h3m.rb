6523799d1247a0580c307e9c5d0a03bebdb7315f
Maxim Andryunin <maxim.andryunin@gmail.com>
2013-06-17 01:40:46 +0700

    Add dummy H3m::Map class.

--- lib/h3m.rb
+++ lib/h3m.rb
@@ -1,5 +1,29 @@
 require "h3m/version"
+require "zlib"
 
 module H3m
   
+  class Map
+    # Map representation
+    #
+    # Example:
+    #   >> map = H3m::Map.new("some-map-file.h3m")
+    #   >> map.version
+    #   => :SoD
+
+    attr_reader :path
+
+    def initialize(path)
+      @path = path
+      @gzip_file = File.new(path)
+    end
+
+    def file
+      @file ||= Zlib::GzipReader.new(@gzip_file)
+    end
+
+    def version
+    end
+  end
+
 end
