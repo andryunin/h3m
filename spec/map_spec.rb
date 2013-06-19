ce03eb373b19077e407cb77bf46ef12330769f12
Maxim Andryunin <maxim.andryunin@gmail.com>
2013-06-19 09:52:23 +0700

    Add RSpecs for H3m::Map class.

--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -1,5 +1,31 @@
 require "spec_helper"
 
 describe H3m::Map do
-  pending "This test should be written first"
+
+  before :all do
+    @files  = Dir.glob("spec/resources/*.h3m")
+    @params = @files.map do |path|
+      filename = File.basename(path, ".h3m")
+      version, size = filename.split("_").map(&:to_sym)
+      {version: version, size: size}
+    end
+
+  end
+
+  it "should return correct version" do
+    @files.each_with_index do |path, index|
+      params = @params[index]
+      map = H3m::Map.new(path)
+      map.version.should == params[:version]
+    end
+  end
+
+  it "should return correct size" do
+    @files.each_with_index do |path, index|
+      params = @params[index]
+      map = H3m::Map.new(path)
+      map.size.should == params[:size]
+    end
+  end
+
 end
