1f723f0b97875d740b0626d0ee368fc4ce96fba0
Maxim Andryunin <maxim.andryunin@gmail.com>
2013-06-25 09:19:49 +0700

    Add specs for expected exceptions.

--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -9,7 +9,14 @@ describe H3m::Map do
       version, size = filename.split("_").map(&:to_sym)
       {version: version, size: size}
     end
+  end
 
+  before :each do
+    # File for testing errors: 10 kb of 0xFF except for the lengths of
+    # map name and descriptions
+    content = "\xFF" * 10240
+    content[10...18] = "\x00" * 8
+    @badfile = StringIO.new(content, "rb")
   end
 
   it "should return correct version" do
@@ -18,6 +25,11 @@ describe H3m::Map do
       map = H3m::Map.new(path)
       map.version.should == params[:version]
     end
+
+    expect {
+      map = H3m::Map.new(@badfile, true)
+      map.version
+    }.to raise_error(H3m::MapError)
   end
 
   it "should return correct size" do
@@ -26,6 +38,11 @@ describe H3m::Map do
       map = H3m::Map.new(path)
       map.size.should == params[:size]
     end
+
+    expect {
+      map = H3m::Map.new(@badfile, true)
+      map.size
+    }.to raise_error(H3m::MapError)
   end
 
   it "should return correct name and description" do
@@ -41,6 +58,11 @@ describe H3m::Map do
 
     map = H3m::Map.new("spec/resources/test_2.h3m")
     map.has_subterranean?.should == false
+
+    expect {
+      map = H3m::Map.new(@badfile, true)
+      map.has_subterranean?
+    }.to raise_error(H3m::MapError)
   end
 
   it "should correctly determine map difficulty" do
@@ -49,6 +71,11 @@ describe H3m::Map do
 
     map = H3m::Map.new("spec/resources/test_2.h3m")
     map.difficulty.should == :impossible
+
+    expect {
+      map = H3m::Map.new(@badfile, true)
+      map.difficulty
+    }.to raise_error(H3m::MapError)
   end
 
 end
