ff087d3f570bbbf94756379f0de41be242ce01ed
Maxim Andryunin <maxim.andryunin@gmail.com>
2013-07-03 23:43:08 +0700

    Change player specs nesting for get name of failed file.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,46 +3,45 @@ require 'yaml'
 
 describe H3m::Player do
 
-  before :all do
-    @fixtures = YAML.load_file("spec/resources.yml")
-    @files = @fixtures.map do |p|
-      {map: H3m::Map.new("spec/resources/#{p["file"]}"), params: p }
-    end
+  @fixtures = YAML.load_file("spec/resources.yml")
+  @files = @fixtures.map do |p|
+    {path: "spec/resources/#{p["file"]}", params: p }
   end
 
-  it "should correcty determine player presence" do
-    @files.each do |file|
-      file[:map].players.each_with_index do |p, i|
-        p.present?.should == file[:params]["players"][i]["present"]
-      end
-    end
-  end
+  @files.each do |file|
+    context "from file #{file[:path]}" do
 
-  it "should correcty determine player availability to human and computer" do
-    @files.each do |file|
-      players = file[:map].players.select {|p| p.present? }
-      players.each_with_index do |p, i|
-        p.human?.should == file[:params]["players"][i]["human"]
-        p.computer?.should == file[:params]["players"][i]["human"]
+      before :all do
+        @map = H3m::Map.new(file[:path])
+        @present_players  = @map.players.select {|p| p.present? }
+        @computer_players = @map.players.select {|p| p.computer? }
       end
-    end
-  end
 
-  it "should correcty determine computer behaviour" do
-    @files.each do |file|
-      players = file[:map].players.select {|p| p.computer? }
-      players.each_with_index do |p, i|
-        p.computer_behaviour.should == file[:params]["players"][i]["computer_behaviour"]
+      it "should determine player presence" do
+        @players.each_with_index do |p, i|
+          p.present?.should == file[:params]["players"][i]["present"]
+        end
       end
-    end
-  end
 
-  it "should correcty determine has player town on start or not" do
-    @files.each do |file|
-      players = file[:map].players.select {|p| p.present? }
-      players.each_with_index do |p, i|
-        p.has_town_on_start?.should == file[:params]["players"][i]["has_town_on_start"]
+      it "should determine player availability to human and computer" do
+        @present_players.each_with_index do |p, i|
+          p.human?.should == file[:params]["players"][i]["human"]
+          p.computer?.should == file[:params]["players"][i]["human"]
+        end
       end
+      
+      it "should determine computer behaviour" do
+        @computer_players.each_with_index do |p, i|
+          p.computer_behaviour.should == file[:params]["players"][i]["computer_behaviour"]
+        end
+      end
+      
+      it "should determine has player town on start or not" do
+        @present_players.each_with_index do |p, i|
+          p.has_town_on_start?.should == file[:params]["players"][i]["has_town_on_start"]
+        end
+      end
+
     end
   end
 
